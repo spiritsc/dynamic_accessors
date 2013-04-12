1058150bcab70de7731ddf6f2c07195dd0851ab8
Szymon Czarnecki <spirit_sc@thinkdifferent.pl>
2013-04-12 17:50:27 +0200

    Time.zone respond...

--- lib/dynamic_accessors/conventers/time_conventer.rb
+++ lib/dynamic_accessors/conventers/time_conventer.rb
@@ -11,11 +11,11 @@ module Conventers
     def execute(value=nil)
       _value = case 
       when value.is_a?(String)
-        Time.parse(value)
+        Time.respond_to?(:zone) ? Time.zone.parse(value) : Time.parse(value)
       when value.is_a?(Time)
-        value
+        Time.respond_to?(:zone) ? Time.zone.parse(value.to_s) : value
       when value.is_a?(Date)
-        Time.parse(value.to_s)
+        Time.respond_to?(:zone) ? Time.zone.parse(value.to_s) : Time.parse(value.to_s)
       end
       utc? ? _value.utc : _value
     rescue
